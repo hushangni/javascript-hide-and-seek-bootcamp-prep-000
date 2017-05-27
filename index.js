ffad54abeed2f0c664aabcc5d8bfa03a04c80284
Shangni Hu <shangnihu@gmail.com>
2017-05-27 14:21:56 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,34 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
+
+function nestedTarget(){
+  return document.querySelector('#nested .target');
+}
+
+function deepestChild(){
+  return document.querySelector('#grand-node div div div div');
+}
+
+function increaseRankBy(n){
+  var lis = document.querySelectorAll(".ranked-list");
+  var first = lis[0];
+  var second = lis[1];
+  var start = 1;
+
+  var children = first.children;
+
+  for (let i = 0; i< children.length; i++) {
+
+    children[i].innerHTML = parseInt(children[i].innerHTML) + n;
+  }
+
+  children = second.children;
+  start = 12;
+
+  for (var i = 0; i< children.length; i++){
+    children[i].innerHTML = parseInt(children[i].innerHTML) + n;
+  }
+
+
+}
